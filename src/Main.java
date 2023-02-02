5ac7cf255f6e972396aadf2763ea761626173b3d
maria-sancho <alusan7934@ieselcaminas.org>
2023-02-02 14:13:31 +0100

    Polynomials

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Polynomial p1 = new Polynomial(1.1, 5.5, 4.4);
+        Polynomial p2 = new Polynomial(1.6, 2.5, 3.4, 4.7, 1.3);
+
+        Polynomial p3 = new Polynomial("poly.txt");
+
+        System.out.println("p1 degree: " + p1.getDegree());
+        System.out.println("p2 degree: " + p2.getDegree());
+        System.out.println("p3 degree: " + p3.getDegree());
+
+        System.out.println("p1: " + p1);
+        System.out.println("p2: " + p2);
+        System.out.println("p3: " + p3);
+
+        System.out.println("p1(10): " + p1.evaluate(10));
+        System.out.println("p2(10): " + p2.evaluate(10));
+        System.out.println("p3(10): " + p3.evaluate(10));
+
+        Polynomial p4 = p1.add(p2);
+        System.out.println("p1+p2: " + p4);
+
+        Polynomial p5 = p1.multiply(p2);
+        System.out.println("p1*p2: " + p5);
+
+    }
 }
--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,2 +1,88 @@
-package PACKAGE_NAME;public class Polynomial {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Polynomial {
+
+    private double[] coeffs;
+
+    public Polynomial(double... coeffs) {
+        this.coeffs = coeffs;
+    }
+
+    public Polynomial(String filename) {
+        Scanner in = null;
+        try {
+            in = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        int degree = in.nextInt();
+        coeffs = new double[degree+1];
+        for (int i=0; i<coeffs.length; ++i) {
+            coeffs[i] = in.nextDouble();
+        }
+    }
+
+    public int getDegree() {
+        return coeffs.length - 1;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i=coeffs.length-1; i>=0; --i) {
+            sb.append(coeffs[i]);
+            if (i > 0) {
+                if (i == 1) {
+                    sb.append("x");
+                } else {
+                    sb.append("x^").append(i);
+                }
+                if (i > 1) {
+                    sb.append("+");
+                }
+            }
+        }
+
+        if (coeffs.length > 3) {
+            sb.insert(sb.length() - 3, "+");
+        }
+
+        return sb.toString();
+    }
+
+    public double evaluate(double x) {
+        double result = 0;
+        for (int i=coeffs.length-1; i>=0; --i) {
+            result = result * x + coeffs[i];
+        }
+        return result;
+    }
+
+    public Polynomial add(Polynomial another) {
+        int resultDegree = Math.max(this.getDegree(), another.getDegree());
+
+        double[] result = new double[resultDegree + 1];
+
+        for (int i=0; i<result.length; ++i) {
+            double thisCoeff = (i < this.coeffs.length) ? this.coeffs[i] : 0;
+            double anotherCoeff = (i < another.coeffs.length) ? another.coeffs[i] : 0;
+            result[i] = thisCoeff + anotherCoeff;
+        }
+
+        return new Polynomial(result);
+    }
+
+    public Polynomial multiply(Polynomial another) {
+        int length = this.coeffs.length + another.coeffs.length - 1;
+
+        double[] result = new double[length];
+
+        for (int i=0; i<this.coeffs.length; ++i) {
+            for (int j=0; j<another.coeffs.length; ++j) {
+                result[i+j] += this.coeffs[i] * another.coeffs[j];
+            }
+        }
+        return new Polynomial(result);
+    }
 }
